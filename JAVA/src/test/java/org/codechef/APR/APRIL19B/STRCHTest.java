d6759d10a5fdcbdd5f4ee69bb5212aa5480b8424
Ashutosh Gupta <as01hu@gmail.com>
2019-04-21 17:12:03 +0530

    APRIL test

---
+++ JAVA/src/test/java/org/codechef/APR/APRIL19B/STRCHTest.java
@@ -0,0 +1,13 @@
+package org.codechef.APR.APRIL19B;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class STRCHTest {
+
+    @Test
+    public void main() {
+        System.out.println("Anything...");
+    }
+}
