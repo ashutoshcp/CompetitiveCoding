e9868c317c6e5d438fab92db1cdcba4f0a0dc01e
Ashutosh Gupta <as01hu@gmail.com>
2019-04-24 00:38:38 +0530

    24th April.

---
+++ JAVA/src/main/java/org/mine/initial/Solution1.java
@@ -0,0 +1,22 @@
+package org.mine.initial;
+
+import org.mine.regex.UsernameValidator;
+
+import java.util.Scanner;
+
+public class Solution1 {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
