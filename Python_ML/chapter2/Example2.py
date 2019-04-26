a04139bb3d81c670695a1f3a2d85eebb312e30d0
Ashutosh Gupta <as01hu@gmail.com>
2019-04-27 02:15:04 +0530

    Chapter 2 done. Chapter 3 init.

--- Python_ML/chapter2/Example2.py
+++ Python_ML/chapter2/Example2.py
@@ -7,7 +7,7 @@ from sklearn.datasets import fetch_20newsgroups
 cv = CountVectorizer(stop_words="english", max_features=500)
 groups = fetch_20newsgroups()
 transformed = cv.fit_transform(groups.data)
-print(cv.get_feature_names())
+# print(cv.get_feature_names())
 
 sns.distplot(np.log(transformed.toarray().sum(axis=0)))
 plt.xlabel('Log Count')
