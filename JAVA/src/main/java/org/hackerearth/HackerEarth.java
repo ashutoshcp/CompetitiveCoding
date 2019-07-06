9a45b54dfc9fa13a1d0a83c74951843c48fc9775
ashutoshcp <as01hu@gmail.com>
2019-07-07 02:00:28 +0530

    up

--- JAVA/src/main/java/org/hackerearth/HackerEarth.java
+++ JAVA/src/main/java/org/hackerearth/HackerEarth.java
@@ -2,29 +2,29 @@ package org.hackerearth;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.Comparator;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.TreeMap;
 
 public class HackerEarth {
-    public static void main(String args[]) {
-        Map<String, Integer> names = new HashMap<>();
-        names.put("Anne", 10);
-        names.put("John", 35);
-        names.put("Bob", 2);
-        Set<Map.Entry<String, Integer>> set = names.entrySet();
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(set);
-        list.sort((order1, order2) -> (order2.getValue()).compareTo(order1.getValue()));
-        for(Map.Entry<String, Integer> entry:list){
-            System.out.print(" "+entry.getKey());
-        }
+    int[] fib = new int[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+    public static void main(String[] args) {
+        String sentence = "Hello hi bi bye.";
+        sentence =  sentence.substring(0, sentence.length()-1);
+        sentence = sentence.toLowerCase();
+        String[] split = sentence.split(" ");
+        StringBuilder solution = new StringBuilder();
+        Map<Integer, List<String>> map = new TreeMap<>();
+        Arrays.stream(split).forEach(s -> {
+            int l = s.length();
+            map.putIfAbsent(l, new ArrayList<>());
+            map.get(l).add(s);
+        });
+        map.forEach((key, value) -> value.forEach(s -> solution.append(s).append(" ")));
+        String ans = solution.toString().trim();
+        ans = ans.substring(0,1).toUpperCase() + ans.substring(1) + ".";
+
+        System.out.println(ans);
     }
 
 }
